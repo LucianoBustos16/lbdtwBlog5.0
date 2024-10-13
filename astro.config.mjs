48a60b02b14c5ac5ab3834917ccaba0a003eac67
Luciano Bustos <bustosluciano89@gmail.com>
2024-10-13 18:36:57 -0300

    clear oatuh

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 
-import auth from 'auth-astro';
-
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()]
+  integrations: [tailwind()]
 });
