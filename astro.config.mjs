09c94f4031c6a30813df6f2f34887994b35fc702
Luciano Bustos <bustosluciano89@gmail.com>
2025-05-13 19:42:22 -0300

    probando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'; // <-- este u otro adaptador SSR
 import tailwind from '@astrojs/tailwind';
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // <-- debe estar en 'server' para SSR
+  adapter: node({ mode: 'standalone' }), // Provide required options
   integrations: [tailwind()]
 });
